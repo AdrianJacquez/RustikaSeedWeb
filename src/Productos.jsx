027e3d3102ed597ffe403de014b213012dd2cdf9
MINGW64 <Adrijesuslerma@outlook.com>
2024-05-29 15:45:53 -0700

    TermineProductos

--- src/Productos.jsx
+++ src/Productos.jsx
@@ -16,42 +16,82 @@ function Productos() {
           <strong>eficiente y sostenible</strong>.
         </p>
         <div id="containerGrid">
-          <div id="contHortalizas">
+          <div className="contRow">
+            <img
+              className="imgProductos"
+              src="https://t4.ftcdn.net/jpg/05/94/67/45/240_F_594674526_7oL1c2DpQH0ceZk78gjKbIxHWzRVNBGK.jpg"
+            ></img>
             <p>
-              <strong>Semillas De Hortalizas </strong> hibridas y variedades
-              adaptables a la mayor parte de las condiciones ambientales de
-              México.
+              <strong>Semillas De Hortalizas </strong> Las semillas de
+              hortalizas híbridas y variedades adaptables representan una
+              innovación significativa en la agricultura moderna, especialmente
+              en un país con la diversidad climática y de suelos como México.
+              Estas semillas están diseñadas para ofrecer un rendimiento
+              superior, combinando características deseables de diferentes
+              variedades parentales para obtener plantas más robustas y
+              productivas.
             </p>
           </div>
-          <div id="contHTomates">
+          <div className="contRowReverse">
+            <img
+              className="imgProductos"
+              src="https://t4.ftcdn.net/jpg/02/15/24/13/240_F_215241343_VMmzRvWDzpqfN5zFn7nEE6Eu7gyTFUHb.jpg"
+            ></img>
             <p>
               <strong>Tomates </strong>
-              Nuestro programa de mejora de tomates se centra en crear híbridos
-              con un vigor vegetal superior, raíces robustas y un excelente
-              conjunto de resistencias.
+              Nuestro programa de mejora de tomates se centra en la creación de
+              híbridos avanzados que combinan vigor vegetal superior, raíces
+              robustas y un excelente conjunto de resistencias. Este enfoque
+              innovador en el desarrollo de nuevas variedades de tomate está
+              diseñado para enfrentar los desafíos actuales de la agricultura,
+              proporcionando a los agricultores plantas que no solo son más
+              productivas, sino también más resistentes a una variedad de
+              factores adversos.
             </p>
           </div>
-          <div id="contChiles">
+          <div className="contRow">
+            <img
+              className="imgProductos"
+              src="https://t3.ftcdn.net/jpg/08/00/24/00/240_F_800240050_EcXZ4dAlB1Za2SiZX0izJkAZJlsmUBOs.jpg"
+            ></img>
             <p>
               <strong>Chiles </strong>
-              Nuestro programa de mejora de chiles se dedica a desarrollar
-              híbridos con gran rusticidad y tamaños grandes, adaptados a las
-              necesidades del mercado en México.
+              La rusticidad en nuestras variedades híbridas de chiles significa
+              que estas plantas pueden prosperar en una amplia gama de
+              condiciones ambientales, desde climas cálidos y secos hasta
+              regiones más húmedas y frías. Esta robustez permite a los
+              agricultores cultivar chiles con confianza, sabiendo que sus
+              plantas pueden resistir factores de estrés como enfermedades,
+              plagas y variaciones climáticas extremas.
             </p>
           </div>
-          <div id="contSemillasGranos">
+          <div className="contRowReverse">
+            <img
+              className="imgProductos"
+              src="https://t4.ftcdn.net/jpg/04/34/27/21/240_F_434272183_bJy3DtMV4Kr0n1MQS2SpfWiwzO5KsYuJ.jpg"
+            ></img>
             <p>
               <strong>Semillas Granos </strong>
-              Tenemos un amplio portafolio de semillas de granos y forrajes
-              adaptables a la mayor parte de las condiciones ambientales.
+              Contamos con un extenso catálogo de semillas de granos y forrajes
+              diseñadas para adaptarse a una amplia variedad de condiciones
+              ambientales. Nuestra dedicación a la diversidad y adaptabilidad
+              significa que nuestros productos pueden prosperar en diferentes
+              entornos, desde regiones áridas hasta zonas más húmedas, y desde
+              suelos arenosos hasta suelos arcillosos.
             </p>
           </div>
-          <div id="contBiofertilizantes">
+          <div className="contRow">
+            <img
+              className="imgProductos"
+              src="https://t3.ftcdn.net/jpg/07/74/85/26/240_F_774852699_NI2BzZ5cPNnilAmYX6YXEE0qVXNfrnNr.jpg"
+            ></img>
             <p>
               <strong>Biofertilizantes </strong>
-              Nos especializamos en comercializar biofertilizantes que mejoran
-              la fertilidad del suelo y promueven un crecimiento saludable de
-              las plantas.
+              En nuestra empresa, nos destacamos por ofrecer una amplia gama de
+              biofertilizantes que están diseñados para mejorar la fertilidad
+              del suelo y fomentar un crecimiento saludable de las plantas.
+              Nuestra especialización en este campo se basa en el compromiso con
+              la agricultura sostenible y la salud del medio ambiente.
             </p>
           </div>
         </div>
